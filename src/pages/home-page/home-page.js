f5df2de5f5409549d662b1681dd41c9d7a53b737
alayu <se.alayu.estifanos@gmail.com>
2022-06-16 07:02:52 +0300

    adding elements for projects component

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -1,65 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import {
-  FaFacebook,
-  FaInstagram,
-  FaTwitter,
-  FaGithub,
-  FaLinkedin,
-} from "react-icons/fa";
 
 import "./home-page.css";
-import NavBar from "../../components/navbar/navbar";
-import HeroImage from "../../assets/images/hero-image.jpg";
 import AboutPage from "../about-page/about-page";
 import Service from "../../components/service/service";
+import Header from "../../components/header/header";
 
 const HomePage = () => {
   return (
     <>
-      <header className="hero">
-        <div className="section-center hero-center">
-          <article className="hero-info">
-            <div className="underline"></div>
-            <h1>i'm alayu</h1>
-            <h4>freelance web & mobile UI/UX Designer</h4>
-            <Link to="contact" className="btn hero-btn">
-              hire me
-            </Link>
-
-            <ul className="social-icons hero-icons">
-              <li>
-                <a href="https://www.twitter.com" className="social-icon">
-                  <FaFacebook />
-                </a>
-              </li>
-              <li>
-                <a href="https://www.twitter.com" className="social-icon">
-                  <FaLinkedin />
-                </a>
-              </li>
-              <li>
-                <a href="https://www.twitter.com" className="social-icon">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li>
-                <a href="https://www.twitter.com" className="social-icon">
-                  <FaInstagram />
-                </a>
-              </li>
-              <li>
-                <a href="https://www.twitter.com" className="social-icon">
-                  <FaGithub />
-                </a>
-              </li>
-            </ul>
-          </article>
-          <article className="hero-img">
-            <img src={HeroImage} className="hero-photo" alt="john doe" />
-          </article>
-        </div>
-      </header>
+      <Header />
       <AboutPage />
       <Service />
     </>
--- src/pages/projects-page/project-page.js
+++ src/pages/projects-page/project-page.js
@@ -1,9 +1,46 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
 
 import "./project-page.css";
+import AboutImage from "../../assets/images/about.jpeg";
 
 const ProjectPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <header class="projects-hero">
+        <div class="section-title">
+          <h1>my projects</h1>
+          <div class="underline"></div>
+        </div>
+      </header>
+      <section class="section">
+        <div class="section-center projects-page-center">
+          <article class="single-project">
+            <div class="project-container">
+              <img src={AboutImage} alt="single project" />
+              <a href="#" class="project-icon">
+                <FaHome />
+              </a>
+            </div>
+            <div class="project-details">
+              <h4>project name</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                eveniet amet odit aperiam, provident cum possimus sapiente minus
+                quos! Ipsum?
+              </p>
+              <div class="project-footer">
+                <span>
+                  <i class="fab fa-github"></i>
+                </span>
+                <a href="https://www.github.com">source code</a>
+              </div>
+            </div>
+          </article>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProjectPage;
