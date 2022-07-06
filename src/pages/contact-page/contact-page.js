b34ef0213e6fbcd96620fbc6db62805712f3940c
alayu <se.alayu.estifanos@gmail.com>
2022-07-06 16:34:14 +0300

    creating contact page

--- src/pages/contact-page/contact-page.js
+++ src/pages/contact-page/contact-page.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./contact-page.css";
 
 const ContactPage = () => {
-  return <div></div>;
+  return (
+    <section class="section single-page">
+      <div class="section-title">
+        <h1>contact</h1>
+        <div class="underline"></div>
+      </div>
+
+      <div class="section-center page-info">
+        <p>
+          If you are looking to get ahold of me, you can send me an email at
+          <a href="mailto:email@email.com">@Alayu's Email</a>
+        </p>
+        <p>
+          You can also reach me on Twitter at
+          <a href="https://www.twitter.com">@Alayu's Twitter </a>
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default ContactPage;
